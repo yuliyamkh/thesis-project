b454d697b3c3523f5d9f084c0b5ad4c7bc933b19
yuliyamkh <yuliya.mkhayan@gmail.com>
2023-07-24 17:06:06 +0200

    add changes in base.py

--- base.py
+++ base.py
@@ -16,16 +16,13 @@ class Agent(ap.Agent):
         # Initial distribution of linguistic variants A and B
         self.memory = np.random.choice(self.p.lingueme,
                                        size=self.p.memory_size,
-                                       p=[self.p.initial_frequency, 1-self.p.initial_frequency])
+                                       p=[0, 1])
 
         # Frequency of A
         self.A = np.count_nonzero(self.memory == 'A') / len(self.memory)
 
         # The produced token
         self.sampled_token = None
-
-        # The ratio of leaders
-        self.n = int(self.p.agents * self.p.n)
 
     def speak(self) -> None:
         """
@@ -40,48 +37,12 @@ class Agent(ap.Agent):
         Reinforce the own behaviour replacing a randomly
         removed token in the memory with the copy of
         the sampled token
-
-        The reinforcement process is preformed according
-        to the three mechanisms of language change:
-        1. neutral change; 2. interactor selection, and
-        3. replicator selection
         """
 
-        # Neutral mechanism
-        if self.p.neutral_change:
-            # Choose a random index to remove
-            random_index = np.random.randint(len(self.memory))
-            # Replace with the sampled token
-            self.memory[random_index] = self.sampled_token
-
-        # Replicator selection and interactor selection mechanisms
-        if self.p.replicator_selection and self.p.interactor_selection:
-            if self.id < self.n:
-                if self.sampled_token == 'A':
-                    if random.random() < self.p.selection_pressure:
-                        # Choose a random index to remove
-                        random_index = np.random.randint(len(self.memory))
-                        # Insert the sampled token
-                        self.memory[random_index] = self.sampled_token
-
-        else:
-            # Replicator selection
-            if self.p.replicator_selection:
-                if self.sampled_token == 'A':
-                    if random.random() < self.p.selection_pressure:
-                        # Choose a random index to remove
-                        random_index = np.random.randint(len(self.memory))
-                        # Replace with the sampled token
-                        self.memory[random_index] = self.sampled_token
-
-            # Interactor selection
-            if self.p.interactor_selection:
-                if self.id < self.n:
-                    if random.random() < self.p.selection_pressure:
-                        # Choose a random index to remove
-                        random_index = np.random.randint(len(self.memory))
-                        # Replace with the token sampled by the neighbour
-                        self.memory[random_index] = self.sampled_token
+        # Choose a random index to remove
+        random_index = np.random.randint(len(self.memory))
+        # Replace with the sampled token
+        self.memory[random_index] = self.sampled_token
 
     def listen(self, neighbour) -> None:
         """
@@ -103,35 +64,24 @@ class Agent(ap.Agent):
             # Replace with the token sampled by the neighbour
             self.memory[random_index] = neighbour.sampled_token
 
-        # Replicator selection and interactor selection
-        if self.p.replicator_selection and self.p.interactor_selection:
-            if self.id > self.n:
-                if neighbour.id <= self.n:
-                    if neighbour.sampled_token == 'A':
-                        if random.random() < self.p.selection_pressure:
-                            # Choose a random index to remove
-                            random_index = np.random.randint(len(self.memory))
-                            # Replace with the token sampled by the neighbour
-                            self.memory[random_index] = neighbour.sampled_token
-        else:
-            # Interactor selection
-            if self.p.interactor_selection:
-                if self.id > self.n:
-                    if neighbour.id <= self.n:
-                        if random.random() < self.p.selection_pressure:
-                            # Choose a random index to remove
-                            random_index = np.random.randint(len(self.memory))
-                            # Replace with the token sampled by the neighbour
-                            self.memory[random_index] = neighbour.sampled_token
-
-            # Replicator selection
-            if self.p.replicator_selection:
-                if self.sampled_token == 'B' and neighbour.sampled_token == 'A':
+        # Interactor selection
+        if self.p.interactor_selection:
+            if self.id > self.p.leaders:
+                if neighbour.id <= self.p.leaders:
                     if random.random() < self.p.selection_pressure:
                         # Choose a random index to remove
                         random_index = np.random.randint(len(self.memory))
                         # Replace with the token sampled by the neighbour
                         self.memory[random_index] = neighbour.sampled_token
+
+        # Replicator selection
+        if self.p.replicator_selection:
+            if self.sampled_token == 'B' and neighbour.sampled_token == 'A':
+                if random.random() < self.p.selection_pressure:
+                    # Choose a random index to remove
+                    random_index = np.random.randint(len(self.memory))
+                    # Replace with the token sampled by the neighbour
+                    self.memory[random_index] = neighbour.sampled_token
 
     def update(self) -> None:
         """
@@ -161,14 +111,29 @@ class LangChangeModel(ap.Model):
         self.network = self.agents.network = ap.Network(self, graph)
         self.network.add_agents(self.agents, self.network.nodes)
 
-        # Change setup of agents
-        # Mechanism: interactor selection
+        # Change setup of agents according to the mechanism operating
+
+        # Mechanisms: Neutral change and replicator selection
+        if self.p.replicator_selection or self.p.neutral_change:
+            # Compute the number of agents who use an innovation
+            num_innovation_agents = int(self.p.initial_frequency * self.p.agents)
+            # Randomly choose the defined number of agents from the population
+            innovation_agents = self.random.sample(self.agents, num_innovation_agents)
+            # Update the memory and the usage frequency of each agent from the subset
+            for agent in innovation_agents:
+                agent.memory = np.random.choice(self.p.lingueme, size=self.p.memory_size, p=[1, 0])
+                agent.A = np.count_nonzero(agent.memory == 'A') / len(agent.memory)
+
+        # Mechanism: Interactor selection
+        # Compute the number of leaders
+        self.p.leaders = int(self.p.agents * self.p.n)
+        # Randomly choose the defined number of leaders from the population
+        leaders = self.random.sample(self.agents, self.p.leaders)
+        # Update the memory and the usage frequency of each agent from the subset
         if self.p.interactor_selection:
-            for agent in self.agents:
-                if agent.id <= agent.n:
-                    agent.memory = np.random.choice(self.p.lingueme,
-                                                    size=self.p.memory_size,
-                                                    p=[1, 0])
+            for agent in leaders:
+                agent.memory = np.random.choice(self.p.lingueme, size=self.p.memory_size, p=[1, 0])
+                agent.A = np.count_nonzero(agent.memory == 'A') / len(agent.memory)
 
     def update(self) -> None:
         """
@@ -225,21 +190,22 @@ class LangChangeModel(ap.Model):
 if __name__ == '__main__':
 
     # Parameters setup
-    parameters = {'agents': 1000,
+    parameters = {'agents': 10,
                   'lingueme': ('A', 'B'),
                   'memory_size': 10,
-                  'initial_frequency': 0.2,
+                  'initial_frequency': 0.5,
                   'number_of_neighbors': 4,
                   'rewiring_probability': 0.01,
                   'interactor_selection': True,
                   'replicator_selection': False,
                   'neutral_change': False,
-                  'selection_pressure': 0.8,
-                  'n': 0.5,
-                  'steps': 10000
+                  'selection_pressure': 0.3,
+                  'n': 0.2,
+                  'leaders': None,
+                  'steps': 1000
                   }
 
     # Perform and plot a specific number of simulations
     # for one parameter set
-    batch_simulate(num_sim=3, model=LangChangeModel, params=parameters)
+    batch_simulate(num_sim=10, model=LangChangeModel, params=parameters)
 
