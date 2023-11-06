f5a671c168c1292c354c5e6575d990420f356551
yuliyamkh <yuliya.mkhayan@gmail.com>
2023-11-06 14:40:35 +0100

    add changes in data_merging.py

--- data_merging.py
+++ data_merging.py
@@ -1,21 +1,30 @@
+import argparse
 import pandas as pd
 import os
 
-parent_directory = "C:/Users/avror/OneDrive/Dokumente/ABM/output_data/output"
-directories_names = os.listdir(parent_directory)
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('--directory', default='output_data', help='Output directory where simulation results are stored')
+arg_parser.add_argument('--mechanism', help='Name of the mechanism')
+arg_parser.add_argument('--out_dir', default='final_output', help='Output directory')
 
 
-def merge(mechanism_name: str):
+def merge(mechanism_name: str, dir_path: str, directories: list, out_dir_path: str) -> None:
     """
-    Merge multiple outputs into one dataframe
-    :param mechanism_name: name of the mechanism
-    :return: dataframe of the .csv format
+    Merge multiple outputs of experiments for a specific
+    mechanism of language change into one output file
+
+    Parameters:
+    -----------
+    mechanism_name:     Name of the mechanism: neutral_change, replicator_selection, or interactor_selection
+    dir_path:           Directory name where the output of the experiments is stored
+    directories:        All directories stored in out_dir
+    out_dir:            The path to the directory where the output should be saved
     """
 
     dfs = []
-    for directory_name in directories_names:
+    for directory_name in directories:
         if directory_name.startswith(mechanism_name):
-            path = os.path.join(parent_directory, directory_name)
+            path = os.path.join(dir_path, directory_name)
             filepath_params = os.path.join(path, "parameters_constants.json")
             filepath_reporters = os.path.join(path, "reporters.csv")
             filepath_params_sample = os.path.join(path, "parameters_sample.csv")
@@ -36,7 +45,7 @@ def merge(mechanism_name: str):
             reporters['population_size'] = population_size
             reporters['rewiring_probability'] = [rewiring_probability] * len(population_size)
             reporters['replicator_selection'] = [mechanism_name == 'replicator_selection'] * len(population_size)
-            reporters['interactor_selection'] = [False] * len(population_size)
+            reporters['interactor_selection'] = [mechanism_name == 'interactor_selection'] * len(population_size)
             reporters['neutral_change'] = [mechanism_name == 'neutral_change'] * len(population_size)
 
             if mechanism_name == 'replicator_selection' or mechanism_name == 'interactor_selection':
@@ -53,12 +62,20 @@ def merge(mechanism_name: str):
     result = pd.concat(dfs, ignore_index=True)
 
     # Check if the directory exists, and create it if it doesn't
-    if not os.path.exists('output'):
-        os.makedirs('output')
+    if not os.path.exists(out_dir_path):
+        os.makedirs(out_dir_path)
 
-    # Save the DataFrame as a CSV and Excel files in the specified directory
-    result.to_csv(os.path.join('output', f'{mechanism_name}_output.csv'))
-    result.to_excel(os.path.join('output', f'{mechanism_name}_output.xlsx'))
+    # Save the DataFrame as a CSV file in the specified directory
+    result.to_csv(os.path.join(out_dir_path, f'{mechanism_name}_output_test.csv'))
 
 
-merge(mechanism_name='neutral_change')
+if __name__ == '__main__':
+    args = arg_parser.parse_args()
+    directory = args.directory
+    dirs = os.listdir(directory)
+    out_dir = args.out_dir
+    mechanism = args.mechanism
+    merge(mechanism_name=mechanism,
+          dir_path=directory,
+          directories=dirs,
+          out_dir_path=out_dir)
